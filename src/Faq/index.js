79e7edffb5533ba851f4ea030b85df87835ad35f
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2023-06-27 11:31:05 +0200

    closes https://github.com/gbif/edna-tool-ui/issues/5

--- src/Faq/index.js
+++ src/Faq/index.js
@@ -5,12 +5,14 @@ import Layout from "../Layout/Layout";
 import PageContent from "../Layout/PageContent";
 import {Collapse, Typography} from "antd"
 import {marked} from "marked"
+import md2json from "md-2-json"
 const {Title} = Typography
 const { Panel } = Collapse;
 
 
 const Faq =  () => {
     const [faq, setFaq] = useState(null)
+    const [markdown, setMarkdown] = useState(null)
     useEffect(() => {
         const getAbout = async () => {
             try {
@@ -22,14 +24,34 @@ const Faq =  () => {
             }
             
         }
-        getAbout()
+       // getAbout()
+
+        const getFaq = async () => {
+          try {
+              const res = await  axios(`/faq.md`)
+
+              setMarkdown(res.data)
+             // console.log(md2json.parse(res.data))
+              //setFaq(res.data)
+             // console.log(res.data)
+          } catch (error) {
+              console.log(error)
+          }
+          
+      }
+      getFaq()
        
 
     },[])
   return (
-    <Layout><PageContent>
+    <Layout><PageContent style={{padding: "48px"}}>
         <Title level={2}>Frequently asked questions</Title>
-       {faq && <Collapse  >
+        {markdown && <span
+                  dangerouslySetInnerHTML={{
+                    __html: marked(markdown),
+                  }}
+                ></span>}
+       {/* faq */ false && <Collapse  >
        {faq.map((item, idx) => <Panel header={item?.q} key={idx}>
         <p>{item?.a}</p>
       </Panel>)}
