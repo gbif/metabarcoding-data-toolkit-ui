5d5aea5006eb87387c7899d2d31edf70a9d2b29f
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-06-08 10:40:42 +0200

    Started autocomplete for organizations

--- src/Publish/OrganisationAutocomplete.js
+++ src/Publish/OrganisationAutocomplete.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import { Row, Col, Typography, Modal, Button, Table, Input, Tag, theme } from "antd"
+import { debounce } from "lodash";
+
+import { Row, Col, Typography, Modal, Button, Table, Input, AutoComplete } from "antd"
 import {
     EditOutlined,
     LinkOutlined
@@ -9,11 +11,38 @@ import config from "../config";
 const { Search } = Input;
 
 
-const OrganisationAutoComplete = () => {
+const OrganisationAutoComplete = ({onSelectOrganisation= () => {}}) => {
 
+    const [options, setOptions] = useState([])
+    const [value, setValue] = useState("")
+    const onSelectOrganisation_ = (val, obj) => {
+        console.log(val)
+        setValue(val)
+        onSelectOrganisation({ key: obj.key, title: val });
+      };
+    const getData = async (q) => {
 
-    return <>Org autocomplete</>
+        try {
+            const res = await axios.get(`${config.backend}/organization/suggest?q=${encodeURIComponent(q)}`)
+            setOptions(res?.data?.result)
+        } catch (error) {
+            
+        }
+    }
+
+    return <AutoComplete
+    onSelect={onSelectOrganisation_}
+    onSearch={getData/* () => debounce(getData, 500) */}
+    options={options}
+    placeholder={"Type to find your organisation"}
+    style={{ width: "100%" }}
+    onChange={setValue}
+    value={value}
+  />
 }
 
 
 export default OrganisationAutoComplete;
+
+
+
