a754888cb7bba06326f3f55d41f07f14334735d6
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-10-10 15:31:35 +0200

    improve login fporm when users enter wrong usename or password

--- src/Auth/LoginForm.js
+++ src/Auth/LoginForm.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Input, Button, Checkbox, Alert, Form } from 'antd';
 import config from '../config'
 const FormItem = Form.Item;
 
-
+const messages = {
+  "Request failed with status code 401": "Invalid User name or Password"
+}
 
 const LoginForm = ({onLogin, invalid}) => {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false)
+  
+  useEffect(()=> {
+    if(!!invalid){
+      setLoading(false)
+    }
+  }, [invalid])
+
   const onFinishFailed = ({ errorFields }) => {
     form.scrollToField(errorFields[0].name);
   };
@@ -34,7 +43,7 @@ const LoginForm = ({onLogin, invalid}) => {
         </Button>
         Or <a href={`https://www.gbif${config.env !== "prod" ? "-uat" : ""}.org/user/profile`}>register at gbif{config.env !== "prod" ? "-uat" : ""}.org now!</a>
       </FormItem>
-      {invalid && <FormItem style={{width: '100%'}}><Alert message={invalid} type="error" /></FormItem>}
+      {invalid && <FormItem style={{width: '100%'}}><Alert message={messages?.[invalid] || invalid} type="error" /></FormItem>}
 
       <FormItem><a className="login-form-forgot" href={`https://www.gbif${config.env !== "prod" ? "-uat" : ""}.org/user/profile`}>Forgot password?</a></FormItem>
     </Form>
